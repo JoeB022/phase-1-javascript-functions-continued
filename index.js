b07037815905c2c1788cd5ac5dc5b1a81a7e86b4
Brian Joe Kamau <joebrian998@gmail.com>
2024-09-30 11:04:05 +0300

    updated files/packages

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// Function to declare the default activity for Saturday
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+
+// Function to wrap an adjective
+function wrapAdjective(wrapper = '*') {
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
+
+// Export the functions for testing
+module.exports = { saturdayFun, mondayWork, wrapAdjective };
